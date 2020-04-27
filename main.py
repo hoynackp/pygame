e066a7fff1fb0a53478b8d37cd05bd8af2125ab2
Parker Hoynacki <hoynackp@student.psdr3.org>
2020-04-27 11:07:24 -0500

    update background

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 import os
+import PIL
 pygame.init()
 
-width, height = 800, 500
+width, height = 800, 450
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('pygame')
+
+bg = pygame.image.load(os.path.join('images', 'bg.jpg')).convert()
+bgX1 = 0
+bgX2 = 450
 
 clock = pygame.time.Clock()
 
