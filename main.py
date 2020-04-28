af6cffa181cbbbcabb560148bdb1dfdc3fadb689
Parker Hoynacki <hoynackp@student.psdr3.org>
2020-04-28 09:57:23 -0500

    add sprite movement

--- main.py
+++ main.py
@@ -11,9 +11,19 @@ bgX1 = 0
 bgX2 = bg.get_width()
 
 class Ninja:
-    running = [pygame.image.load(os.path.join('images', str(x) + '.png')).convert() for x in range(10)]
-    jumping = [pygame.image.load(os.path.join('images', str(x) + '.png')).convert() for x in range(10,20)]
-    sliding = [pygame.image.load(os.path.join('images', str(x) + '.png')).convert() for x in range(20,30)]
+    run_sprites = [pygame.image.load(os.path.join('images', str(x) + '.png')).convert_alpha() for x in range(10)]
+    jump_sprites = [pygame.image.load(os.path.join('images', str(x) + '.png')).convert_alpha() for x in range(10,20)]
+    slide_sprites = [pygame.image.load(os.path.join('images', str(x) + '.png')).convert_alpha() for x in range(20,30)]
+    one = [1]*6
+    two = [2]*12
+    three = [3]*12
+    four = [4]*12
+    zero = [0]*25
+    negone = [-1]*6
+    negtwo = [-2]*12
+    negthree = [-3]*12
+    negfour = [-4]*12
+    jumpList = (one+two+three+four+zero+negone+negtwo+negthree+negfour)
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -25,26 +35,45 @@ class Ninja:
         self.slideCount = 0
         self.jumpCount = 0
         self.runCount = 0
-        jumpList = [1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4]
     def draw(self, screen):
         if self.jumping:
-            self.y -= self.jumpList[self.jumpCount] * 1.3
-            screen.blit(self.jumping[self.jumpCount//12]), (self.x, self.y)
+            self.y -= self.jumpList[self.jumpCount] * 1.2
+            screen.blit(self.jump_sprites[round(self.jumpCount//11)], (self.x, self.y))
             self.jumpCount += 1
             if self.jumpCount > 108:
                 self.jumpCount = 0
-                self.jumping
-        elif self.sliding:
+                self.jumping = False
+                self.runCount = 0
+        elif self.sliding or self.sliding2:
+            if self.slideCount < 20:
+                self.y += 1
+            elif self.slideCount == 109:
+                self.y -= 19
+                self.sliding = False
+                self.sliding2 = True
+            if self.slideCount >= 110:
+                self.slideCount = 0
+                self.sliding2 = False
+                self.runCount = 0
+            screen.blit(self.slide_sprites[round(self.slideCount//11.1)], (self.x,self.y))
+            self.slideCount += 1
         else:
+            if self.runCount > 32:
+                self.runCount = 0
+            screen.blit(self.run_sprites[round(self.runCount//3.3)], (self.x,self.y))
+            self.runCount += 1
 
 clock = pygame.time.Clock()
 
 def updateScreen():
     screen.blit(bg, (bgX1,0))
     screen.blit(bg, (bgX2,0))
+    player.draw(screen)
     pygame.display.update()
 
-fps = 30
+player = Ninja(200, 40, 10, 10)
+
+fps = 60
 run = True
 while run:
     updateScreen()
@@ -60,4 +89,12 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE] or keys[pygame.K_UP]:
+        if not(player.jumping):
+            player.jumping = True
+    if keys[pygame.K_DOWN]:
+        if not(player.sliding):
+            player.sliding = True
 pygame.quit()
